d4aad1db524554c9bca21891a2586bbeff2c4504
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-14 00:42:18 +0100

    mui tested

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
+      <Typography>This is quite okay</Typography>
       <Button variant='contained'>hello</Button>
     </div>
   );
