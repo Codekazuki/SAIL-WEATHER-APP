f86a32000f99c53c185db1fe1a9c3a6d09066a2e
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-15 15:02:49 +0100

    weather cont

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import { Button, Typography } from "@mui/material";
+import { Button, Card, Typography } from "@mui/material";
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
-      <Typography>This is quite okay</Typography>
+      <Typography>This is Sail weather app</Typography>
       <Button variant='contained'>hello</Button>
+      <Button variant='contained'> Fetch Weather </Button>
+      <Card>
+        <Typography sx={{ height: 145, backgroundColor: "red" }}>
+          This is Lagos
+        </Typography>
+      </Card>
     </div>
   );
 }
