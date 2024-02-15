b2f70a57be07c086579b253b291fe43f7176d629
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-15 15:38:58 +0100

    error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Button, Card, Typography } from "@mui/material";
+import AccessibilityIcon from "@mui/icons-material/Accessibility";
 import "./App.css";
 
 function App() {
@@ -7,10 +8,14 @@ function App() {
       <Typography>This is Sail weather app</Typography>
       <Button variant='contained'>hello</Button>
       <Button variant='contained'> Fetch Weather </Button>
-      <Card>
+      <Card sx={{ height: 285 }}>
         <Typography sx={{ height: 145, backgroundColor: "red" }}>
           This is Lagos
         </Typography>
+        <Typography sx={{ color: "green" }}>
+          This is Sail weather app
+        </Typography>
+        <AccessibilityIcon />
       </Card>
     </div>
   );
